f0983d2ba3ee8e83410ddb70cfcafaa722355f4f
Boris Bartelsman <borisbartelsman@gmail.com>
2020-06-23 16:20:00 +0200

    add clean functions

--- data/process_data.py
+++ data/process_data.py
@@ -4,11 +4,12 @@ import pandas as pd
 
 
 def load_data(messages_filepath, categories_filepath):
-   """ Args in: filepath for messages,filtepath for categories.
-       Args out: df
-       Description: Functions takes to filepaths, reads in csv's and merges on index """
-   
-   
+    """ 
+    Args in: filepath for messages,filtepath for categories.
+    Args out: df
+    Description: Functions takes to filepaths, reads in csv's and merges on index
+    """
+    
     # read messages and categorie from file path from file paths
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
@@ -20,7 +21,37 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-    pass
+    """
+    Args in: df
+    Agrs out: df
+    Description: Takes a df, cleans categories column and splits it to separate columns, returns this df
+    """
+
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat = ";", expand = True)
+
+    # rename the columns of `categories`
+    row = categories[:1].copy()
+    category_colnames = pd.np.array(row.applymap(lambda x: x[:-2])).tolist()[0]
+    categories.columns = category_colnames
+
+    # convert catergoy values to numbers 0 or 1
+
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).apply(lambda x: x[-1:]).astype('int64')
+
+    # drop the original categories column from `df`
+    df.drop('categories', inplace = True, axis = 1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = df.merge(categories, left_index = True, right_index = True)
+
+    # drop duplicate values
+    df.drop_duplicates(subset = list(df.columns)[1:], inplace = True)
+
+    return df
+
 
 
 def save_data(df, database_filename):
