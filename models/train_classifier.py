f9bc69a7c1a54e83a75fef70ac68e880e9aae76c
Boris Bartelsman <borisbartelsman@gmail.com>
2020-06-24 10:22:51 +0200

    add def tokenize

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -28,7 +28,30 @@ def load_data(database_filepath):
     df = pd.read_sql_table(database_filepath[:-3], engine)
 
 def tokenize(text):
-    pass
+    """
+    Args in: text file
+    Args out: clean lemmatized tokens (words)
+    Description: Takes text, splits text in words, deletels urls and lemmatizes these words 
+    """
+
+    # finds urls and replaces with placeholder
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    # tokenize and lemmatize
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
 
 
 def build_model():
